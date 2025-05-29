dc59bb4584b67009de66b1dca27506cd841f528f
EnderTerrariaGamerPro <ga.altamirano@duocuc.cl>
2025-05-29 19:59:20 -0400

    Delete Proyecto perfulandia-mish directory
    Tralala

--- Proyecto perfulandia-mish/perfulandia-backend-01V-master/gestionservice/src/main/java/com/perfulandia/gestionservice/repository/SucursalRepository.java
+++
@@ -1,8 +0,0 @@
-package com.perfulandia.gestionservice.repository;
-
-import com.perfulandia.gestionservice.model.Sucursal;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SucursalRepository extends JpaRepository<Sucursal, Long> {
-
-}
