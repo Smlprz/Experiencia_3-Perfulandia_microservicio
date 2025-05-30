7984a1097712feddc67388b34e0fdc10323dd2f3
EnderTerrariaGamerPro <ga.altamirano@duocuc.cl>
2025-05-29 20:00:06 -0400

    Delete main/Proyecto perfulandia-mish/perfulandia-backend-01V-master/GestionService directory

--- main/Proyecto perfulandia-mish/perfulandia-backend-01V-master/GestionService/src/main/java/com/perfulandia/gestionservice/repository/SucursalRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gestion.gestionservice.repository;
-
-import com.gestion.gestionservice.model.Sucursal;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SucursalRepository extends JpaRepository<Sucursal, String> {
-
-}
