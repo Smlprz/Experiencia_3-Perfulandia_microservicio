c2c25c21042dd75581074b8b9cb5f6b8df88de13
vicenteoyarzun <163044338+vicenteoyarzun@users.noreply.github.com>
2025-07-03 20:41:55 -0400

    Delete main/Proyecto perfulandia-mish/perfulandia-backend-01V-master/AutenticacionService directory

--- main/Proyecto perfulandia-mish/perfulandia-backend-01V-master/AutenticacionService/AutenticacionService/src/main/java/com/perfulandia/AutenticacionService/AutenticacionService/repository/AutenticacionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.perfulandia.AutenticacionService.AutenticacionService.repository;
-import com.perfulandia.AutenticacionService.AutenticacionService.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-public interface AutenticacionRepository extends JpaRepository<Usuario, Long> {
-    Optional<Usuario> findByCorreo(String correo);
-}
